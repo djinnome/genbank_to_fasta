1e687d42851889a6a2417a642efb1edf5deb67fd
Vlad Dracula <djinnome@gmail.com>
2018-01-11 14:47:13 -0800

    Added default file explanation to README

--- genbank_to_faa.py
+++ genbank_to_faa.py
@@ -22,8 +22,8 @@ def convert_genbank_to_faa( gbk_filename, faa_filename, qualifier='label') :
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract protein fasta from Genbank files')
-    parser.add_argument('genbank', nargs='?', type=argparse.FileType('r'), default = sys.stdin, help='input genbank file')
-    parser.add_argument('protein_fasta', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='output fasta file')
-    parser.add_argument('--qualifier', nargs='?', default='label', help='Genbank qualifier used for the fasta header' )
+    parser.add_argument('genbank', nargs='?', type=argparse.FileType('r'), default = sys.stdin, help='input genbank file. Default: STDIN')
+    parser.add_argument('protein_fasta', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='output fasta file. Default: STDOUT')
+    parser.add_argument('--qualifier', nargs='?', default='label', help='Genbank qualifier used for the fasta header. Default: label' )
     args = parser.parse_args()
     convert_genbank_to_faa( args.genbank, args.protein_fasta, args.qualifier )
